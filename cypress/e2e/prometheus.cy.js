4e57c951c3c4ceedb48f2ce0812186f0b04455fc
Tobias Gad Spoorendonk <tosp@itu.dk>
2023-03-15 18:01:16 +0100

    Added: new test to check the validity of PromQL statements

--- cypress/e2e/prometheus.cy.js
+++ cypress/e2e/prometheus.cy.js
@@ -1,3 +1,11 @@
+describe('PromQL statements', () => {
+  it('checks the health of prometheus', () => {
+    cy.request('localhost:9090/api/v1/query?query=count(device_health<0.01)').its('body').then((body) => {
+      expect(body.data.result[0].value[0]).to.be.greaterThan(0)
+    })
+  })
+})
+
 describe('Prometheus health check', () => {
   it('checks the health of prometheus', () => {
     cy.request('localhost:9090/-/healthy').its('body').should('include', 'Prometheus Server is Healthy.')
