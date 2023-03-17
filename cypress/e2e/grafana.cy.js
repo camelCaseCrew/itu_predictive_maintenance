5a07e7dc0ac5c6f241598df6909f9a142c8c48a7
adamjhr <adam.jh.rose6@gmail.com>
2023-03-17 12:02:48 +0100

    Added: Tests to see if grafana configuration works properly

--- cypress/e2e/grafana.cy.js
+++ cypress/e2e/grafana.cy.js
@@ -5,3 +5,27 @@ describe('Grafana health check', () => {
       })
   })
 })
+
+describe('Check configuration', () => {
+  it('Checks that configuration files configure grafana correctly', () => {
+    cy.request({
+      url: 'localhost:3000/api/datasources',
+      auth: {
+        'bearer': 'eyJrIjoiTEx1NmVvcHY3NTk2dEhPQ042Z1R0NE5DN0NVbkZ1YXkiLCJuIjoiY3lwcmVzcyIsImlkIjoxfQ=='
+      }}
+      ).its('body').then((body) => {
+      expect(body[0].uid).to.equal('TSwyULa4z')
+      expect(body[0].name).to.equal('Prometheus')
+    })
+
+    cy.request({
+      url: 'localhost:3000/api/dashboards/uid/en2yCsa4k',
+      auth: {
+        'bearer': 'eyJrIjoiTEx1NmVvcHY3NTk2dEhPQ042Z1R0NE5DN0NVbkZ1YXkiLCJuIjoiY3lwcmVzcyIsImlkIjoxfQ=='
+      }}).its('body').then((body) => {
+      cy.log(body)
+      expect(body.dashboard.uid).to.equal('en2yCsa4k')
+      expect(body.dashboard.title).to.equal('Overview of devices')
+    })  
+  })
+})
