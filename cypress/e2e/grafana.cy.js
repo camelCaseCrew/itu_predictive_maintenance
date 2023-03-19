dd15b785120849263bf6d2826ff7cd4525da4ae1
jfho <jfho@itu.dk>
2023-03-19 18:51:42 +0100

    Changed: Grafana test to use basic auth

--- cypress/e2e/grafana.cy.js
+++ cypress/e2e/grafana.cy.js
@@ -2,30 +2,17 @@ describe('Grafana health check', () => {
   it('checks the health of Grafana', () => {
     cy.request('localhost:3000/api/health').its('body').then((body) => {
       expect(body.database).to.eq('ok')
-      })
+    })
   })
 })
 
 describe('Check configuration', () => {
   it('Checks that configuration files configure grafana correctly', () => {
-    cy.request({
-      url: 'localhost:3000/api/datasources',
-      auth: {
-        'bearer': 'eyJrIjoiTEx1NmVvcHY3NTk2dEhPQ042Z1R0NE5DN0NVbkZ1YXkiLCJuIjoiY3lwcmVzcyIsImlkIjoxfQ=='
-      }}
-      ).its('body').then((body) => {
-      expect(body[0].uid).to.equal('TSwyULa4z')
+    cy.request('http://admin:admin@localhost:3000/api/datasources').its('body').then(body => {
       expect(body[0].name).to.equal('Prometheus')
     })
-
-    cy.request({
-      url: 'localhost:3000/api/dashboards/uid/en2yCsa4k',
-      auth: {
-        'bearer': 'eyJrIjoiTEx1NmVvcHY3NTk2dEhPQ042Z1R0NE5DN0NVbkZ1YXkiLCJuIjoiY3lwcmVzcyIsImlkIjoxfQ=='
-      }}).its('body').then((body) => {
-      cy.log(body)
-      expect(body.dashboard.uid).to.equal('en2yCsa4k')
+    cy.request('http://admin:admin@localhost:3000/api/dashboards/uid/en2yCsa4k').its('body').then(body => {
       expect(body.dashboard.title).to.equal('Overview of devices')
-    })  
+    })
   })
 })
