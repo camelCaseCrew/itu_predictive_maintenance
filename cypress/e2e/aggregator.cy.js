d616039f74a020beb00f77af4a7b96dfc8a92689
jfho <jfho@itu.dk>
2023-03-29 20:59:07 +0200

    Changed: Fixed unit test bugs

--- cypress/e2e/aggregator.cy.js
+++ cypress/e2e/aggregator.cy.js
@@ -8,7 +8,7 @@ describe('Data aggregator health check', () => {
 
 describe('Metrics check', () => {
     it('Checks that the correct metrics are shown', () => {
-        cy.request('http://localhost:8001/metrics')
+        cy.request('http://localhost:8003/metrics')
         .its('body')
         .should('contain', 'device_health')
     })
