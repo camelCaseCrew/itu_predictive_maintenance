5c7b6297e51f721a6ca792ed1a521d32ec271d54
Cromski <jakobrhenriksen@outlook.com>
2023-03-29 13:55:34 +0200

    Added: Loop through array, show X many graphs from grafana

--- frontend/src/pages/test.tsx
+++ frontend/src/pages/test.tsx
@@ -1,12 +1,25 @@
+import { useEffect, useState } from "react"
 
 export default function test() {
+
+  const [emptyArray, setEmptyArray] = useState<undefined[]>([...Array(3)])
+
+  console.log(emptyArray)
+
+
   return (
     <>
-      <h3 className=" font-bold text-green-700" > hello there</h3>
-      <div>
-      <iframe src="http://localhost:3000/d-solo/en2yCsa4k/overview-of-devices?orgId=1&from=1679499012553&to=1679520612553&panelId=2"></iframe>
-      </div>
+      <h3 className=" font-bold text-gray-700 text-center" > Health Graphs</h3>
 
+      <div>
+        {emptyArray.map((_, index) => {
+          return (
+            <iframe key={index} loading="eager" src={`http://localhost:3000/d-solo/en2yCsa4k/overview-of-devices?orgId=1&var-risk_group=healthy&var-serial_number=All&panelId=${index+12}&refresh=15s`} width="450" height="200"></iframe>
+          )
+        })
+        }
+        
+      </div>
     </>
   )
 }
