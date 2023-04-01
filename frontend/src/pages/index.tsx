c93d71805ec5cd866f42c6ed71821b0ea7dcc714
Danyal Yorulmaz <skoledanyal@gmail.com>
2023-04-01 15:15:47 +0200

    Changed: Location of navbar from index page to the app itself so it persists on all routes

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,8 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +13,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Navbar />
     </>
   )
 }
