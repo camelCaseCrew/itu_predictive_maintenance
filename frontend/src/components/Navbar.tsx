0c1bc959a30ed0133ed2f72b43cdff821c18c6d5
Danyal Yorulmaz <skoledanyal@gmail.com>
2023-04-01 15:13:24 +0200

    Changed: So that the navbar now uses the custom navbarbutton component

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import Logo from './Logo';
+import NavbarButton from './NavbarButton';
 
 function Navbar() {
     return <div className="bg-component1 text-text w-full h-28 flex justify-start place-items-center gap-x-4">
         <Logo></Logo>
-        <div>
-            Dashboard
-        </div>
-        <div>
-            History
-        </div>
+        <NavbarButton title="Dashboard" href="/dashboard"></NavbarButton>
+        <NavbarButton title="History" href="/history"></NavbarButton>
     </div>
 }
 
