1bdfcb61b4fe0845ff02ec2b2b6f3602938b8b55
adamjhr <adam.jh.rose6@gmail.com>
2023-04-12 16:19:55 +0200

    Added: html select tag with event handler

--- frontend/src/pages/test.tsx
+++ frontend/src/pages/test.tsx
@@ -7,6 +7,10 @@ export default function test() {
     console.log('You clicked me!');
     updateGrafanaSrc(`http://localhost:3000/d/enayayaya/health-graphs?orgId=1&refresh=5s&var-risk_group=risk&var-serial_number=All&kiosk`)
   }
+
+  function riskGroupSelect(value: string) {
+    console.log(value)
+  }
     
   if(typeof window !== "undefined" && window.document){
     window.addEventListener("blur", function (e) {
@@ -22,6 +26,12 @@ export default function test() {
 
 
       <button className="bg-component1 text-text w-full h-28 flex justify-start place-items-center gap-x-4" type="button" onClick={handleClick}>Click me</button>
+
+      <select name="Health" onChange={e => riskGroupSelect(e.target.value)}>
+        <option value="healthy">Healthy</option>
+        <option value="risk">Risk</option>
+        <option value="critical">Critical</option>
+      </select>
 
       <div className=" w-screen h-screen">
       <iframe className="w-full h-full" loading="lazy" src={grafanaSrc} ></iframe>
