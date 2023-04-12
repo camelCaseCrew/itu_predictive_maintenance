3eddf869bfcc33fc541c2f0d6a9afc03a4efb81f
Danyal Yorulmaz <skoledanyal@gmail.com>
2023-04-12 22:46:30 +0200

    Added: Shadow to the button to make it look more like the design guidelines

--- frontend/src/components/NavbarButton.tsx
+++ frontend/src/components/NavbarButton.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 
 function NavbarButton(props: { title: string, href: string }) {
-    return <div className="bg-component2 text-text w-40 h-10 gap-x-4">
+    return <div className="bg-component2 text-text w-48 h-10 gap-x-4 hover:scale-110 transition duration-300 shadow-xl">
         <Link href={props.href} className='h-full w-full flex justify-center place-items-center'>
             {props.title}
         </Link>
