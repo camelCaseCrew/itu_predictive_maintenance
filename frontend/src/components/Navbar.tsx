7d42843dfa8d45f2d86ab4b71fa84bf00fa6e866
eikbo <eikb@itu.dk>
2023-04-12 17:53:03 +0200

    Removed: Experimental Divs

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -5,12 +5,9 @@ import NavbarButton from './NavbarButton';
 function Navbar() {
     return <div className="bg-component1 text-text w-full h-28 flex justify-start place-items-center gap-x-4">
         <Logo></Logo>
-        <div className="bg-component2 text-text w-28 h-28 flex justify-start place-items-center gap-x-4">
         <NavbarButton title="Dashboard" href="/dashboard"></NavbarButton>
-        </div>
-        <div className="bg-component2 text-text w-28 h-28 flex justify-start place-items-center gap-x-4">
         <NavbarButton title="History" href="/history"></NavbarButton>
-        </div>
+    
     </div>
 }
 
