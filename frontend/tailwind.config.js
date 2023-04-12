0ff989739e990420e4b65432c9adcf47c73b4138
jfho <jfho@itu.dk>
2023-04-12 15:51:17 +0200

    Added: Font and design following design guidelines

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,7 +9,17 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background': '#181B1F',
+        'component1': '#22252B',
+        'component2': '#30343D',
+        'text': '#DEDEDE',
+        'ok': '#37872D',
+        'risk': '#FADE2A ',
+        'critical': '#C4162A',
+      },
+    },
   },
   plugins: [],
 }
