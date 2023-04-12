52091448dd70300e58e2d1969957467544d62de3
Cromski <jakobrhenriksen@outlook.com>
2023-04-12 14:32:28 +0200

    Changed: from multiple iframes to use only one iframe for all health graphs

--- frontend/src/pages/test.tsx
+++ frontend/src/pages/test.tsx
@@ -1,26 +1,28 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 export default function test() {
 
   const [emptyArray, setEmptyArray] = useState<undefined[]>([...Array(6)])
 
-  console.log(emptyArray)
-
+  console.log(emptyArray) 
+    
+  if(typeof window !== "undefined" && window.document){
+    window.addEventListener("blur", function (e) {
+      setTimeout(function () {
+        window.focus();
+      }, 0);
+    });
+  }
 
   return (
     <>
       <h3 className=" font-bold text-gray-700 text-center" > Health Graphs</h3>
 
-      <div className="flex row justify-center gap-4 flex-wrap">
-        {emptyArray.map((_, index) => {
-          return (
-            <div className=" w-[23%] h-72">
-            <iframe className=" w-full h-full" key={index} loading="lazy" src={`http://localhost:3000/d-solo/en2yCsa4k/overview-of-devices?orgId=1&var-risk_group=healthy&var-serial_number=All&panelId=${index+12}&refresh=60s`}></iframe>
-            </div>
-          )
-        })}
-        
+
+      <div className=" w-screen h-screen">
+        <iframe  id="devices" className=" w-full h-full" loading="lazy" src={`http://localhost:3000/d/enayayaya/overview-of-devices-copy?orgId=1&refresh=60s&kiosk`}></iframe>
       </div>
+
     </>
   )
 }
