2a06495f061a90a2644b61204e3319845605d76f
jfho <jfho@itu.dk>
2023-04-18 15:13:34 +0200

    Added: filtering test now works

--- cypress/e2e/frontend.cy.js
+++ cypress/e2e/frontend.cy.js
@@ -1,14 +1,32 @@
+import 'cypress-iframe';
+
 describe('Preliminary front end test', () => {
     it('Visits the frontend website', () => {
       cy.visit('http://localhost:3001/test') 
-      cy.contains('hello there') // text on the website
     })
   })
 
 describe('Grafana Graph test, will break if test page is removed', () => {
     it('Visits the test page', () => {
       cy.visit('http://localhost:3001/test')
-      cy.get('iframe[src="http://localhost:3000/d-solo/en2yCsa4k/overview-of-devices?orgId=1&from=1679499012553&to=1679520612553&panelId=2"]').should('exist'); 
+      cy.get('iframe[src="http://localhost:3000/d/enayayaya/overview-of-devices-copy?orgId=1&refresh=60s&kiosk"]').should('exist'); 
       // ^hardcoded for the homepage graph^
     })
+})
+
+describe('Filtering buttons works', () => {
+  it('Checks that filtering buttons have been created', () => {
+    cy.visit('http://localhost:3001/test')
+
+    // check filtering buttons exist
+    cy.get('select[name="Health"]').should('exist')
+    cy.get('input[type="text"]').should('exist')
+
+    // try to filter
+    cy.get('select[name="Health"]').select('Critical')
+    cy.get('input[type="text"]').type('aoao{enter}')
+    
+    cy.frameLoaded('iframe')
+    cy.iframe().find('div[class="panel-title"]').find('h2').should('contain', 'aoao')
   })
+})
