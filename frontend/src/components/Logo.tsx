834b41189fdc88d96cfcd3329eee676a4cd946e8
Cromski <jakobrhenriksen@outlook.com>
2023-04-19 09:54:32 +0200

    Added: Systematic above our logo

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -5,6 +5,7 @@ function Logo() {
     return <div className='m-6 text-bold text-4xl'>
         <Link href="/">
             <h1>PredictIT</h1>
+            <h1 className=' italic absolute -mt-4 font-semibold'>Systematic</h1>
         </Link>
     </div>
 }
