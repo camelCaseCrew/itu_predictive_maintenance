c63291811082ad5bd9f4d6e0644ed5e3e5f009be
Cromski <jakobrhenriksen@outlook.com>
2023-04-19 09:55:01 +0200

    Changed: styling and some syntax

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Link from 'next/link';
 
-function Logo() {
-    return <div className='m-6 text-bold text-4xl'>
+const Logo = () => (
+    <div className='m-6'>
         <Link href="/">
-            <h1>PredictIT</h1>
             <h1 className=' italic absolute -mt-4 font-semibold'>Systematic</h1>
+            <h1 className=' text-4xl'>PredictIT</h1>
         </Link>
     </div>
-}
-
-export default Logo;
+)
