7cc68b00f5d0cde5b991b3cb71d007deadedb445
adamjhr <adam.jh.rose6@gmail.com>
2023-04-19 15:05:52 +0200

    Added: Custom dropdown with styling

--- frontend/src/pages/test.tsx
+++ frontend/src/pages/test.tsx
@@ -1,9 +1,16 @@
 import { useState } from "react"
+import React from 'react'
+import Select from 'react-select'
 
 export default function test() {
   const [grafanaSrc, updateGrafanaSrc] = useState(`http://localhost:3000/d/enayayaya/overview-of-devices-copy?orgId=1&refresh=60s&kiosk`)
   const [riskGroup, updateRiskGroup] = useState('healthy')
-
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ]
+  
   var serialNumber = 'All'
 
   function riskGroupSelect(value: string) {
@@ -32,7 +39,8 @@ export default function test() {
   }
 
   return (
-    <div className="bg-component1 m-0">
+    
+    <div className="m-0">
         <select className="bg-component2 text-text rounded m-4 py-2 px-2" name="Health" onChange={e => riskGroupSelect(e.target.value)}>
           <option value="healthy">Healthy</option>
           <option value="risk">At Risk</option>
@@ -40,6 +48,20 @@ export default function test() {
         </select>
 
         <input className="bg-component2 text-text rounded py-2 px-2" type="text" placeholder="All" onChange={e => serialNumber = e.target.value} onKeyDown={e => serialSearchKeyDown(e.key)}></input>
+
+        <div className="w-60">
+        <Select classNamePrefix="bg-component2 text-text outline-0 " options={options} isMulti styles={{ menu: (base) => ({ ...base, backgroundColor: "#FF0000" }),
+                                                    valueContainer: (base) => ({ ...base, borderTopLeftRadius: '5px', borderBottomLeftRadius: '5px'  }),
+                                                    indicatorsContainer: (base) => ({ ...base, borderTopRightRadius: '5px', borderBottomRightRadius: '5px'  }),
+                                                    dropdownIndicator: (base) => ({ ...base, borderTopRightRadius: '5px', borderBottomRightRadius: '5px'  }),
+                                                    container: (base) => ({ ...base, borderRadius: '5px' }),
+                                                    multiValueLabel: (base) => ({ ...base, color: '#DEDEDE' }), 
+                                                    multiValue: (base) => ({ ...base, backgroundColor: '#30343D' }),
+                                                    control: (base) => ({ ...base, backgroundColor: '#30343D', borderRadius: '5px', border: 'none' }),}}/>
+        
+        </div>
+        
+
 
         <div className=" w-screen h-screen">
         <iframe className="w-full h-full" loading="lazy" src={grafanaSrc} ></iframe>
