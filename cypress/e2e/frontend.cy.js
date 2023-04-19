93f6d0a492ecd95aba9733e431ca13f2d850a99b
Cromski <jakobrhenriksen@outlook.com>
2023-04-19 14:46:49 +0200

    Changed: Test to test on all dimensions from the dimensions.js file

--- cypress/e2e/frontend.cy.js
+++ cypress/e2e/frontend.cy.js
@@ -1,19 +1,24 @@
 import 'cypress-iframe';
 
+const dimensions = require('../dimensions.js')
+
 describe('Check that each row has Max 4 graphs on health_graphs page', () => {
+  Object.values(dimensions).map((key, i) => {
     it('Visits the frontend website', () => {
-      cy.visit('http://localhost:3003/health_graphs') 
+      cy.viewport(key.viewportWidth, key.viewportHeight)
+      cy.visit('http://localhost:3001/health_graphs') 
       var width = cy.config().viewportWidth
       cy.frameLoaded('iframe')
       //This tests that the width of a single Graph is larger than 1/5 of the screen, since this means that
       //The tightest they can be group is in 4 per row
       cy.iframe().find('section[class="panel-container"]').invoke('width').should('be.gt', width/5)
     })
-  })
+  })    
+})
 
 describe('Grafana Graph test, will break if health_graphs page is removed', () => {
     it('Visits the test page', () => {
-      cy.visit('http://localhost:3003/health_graphs')
+      cy.visit('http://localhost:3001/health_graphs')
       cy.get('iframe[src="http://localhost:3000/d/enayayaya/health-graphs?orgId=1&refresh=60s&kiosk"]').should('exist'); 
     })
   })
