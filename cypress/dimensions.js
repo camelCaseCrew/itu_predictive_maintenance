018c1f11fd50b8471b2a276ae867328ec24160b5
Cromski <jakobrhenriksen@outlook.com>
2023-04-19 14:45:44 +0200

    Added: Dimensions.js which has all dimensions we can make cypress tests on

---
+++ cypress/dimensions.js
@@ -0,0 +1,12 @@
+module.exports = {
+    //desktop screens
+    normal: {
+        viewportWidth: 1920,
+        viewportHeight: 1080
+    },
+    //phone screens
+    phone: {
+        viewportWidth: 375,
+        viewportHeight: 812
+    },
+}
