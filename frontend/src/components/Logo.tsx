19b544fc906295efae02214d3ac123ee59395e06
Cromski <jakobrhenriksen@outlook.com>
2023-04-19 12:18:49 +0200

    Added: Id to Logo

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 
 const Logo = () => (
-    <div className='m-6'>
+    <div id='Logo-id' className='m-6'>
         <Link href="/">
             <h1 className=' italic absolute -mt-4 font-semibold'>Systematic</h1>
             <h1 className=' text-4xl'>PredictIT</h1>
