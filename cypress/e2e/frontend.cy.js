949eca841cf7a3567300c4d1b3b69dd78a047840
adamjhr <adam.jh.rose6@gmail.com>
2023-04-20 11:25:46 +0200

    Changed: Test visit url to port 3001 instead of 3003

--- cypress/e2e/frontend.cy.js
+++ cypress/e2e/frontend.cy.js
@@ -2,7 +2,7 @@
 
 describe('Presence of Logo', () => {
   it('Checks for the presence of "Systematic" and "PredictIT"', () => {
-    cy.visit('http://localhost:3003')
+    cy.visit('http://localhost:3001')
     cy.contains('Systematic')
     cy.contains('PredictIT') 
   })
@@ -10,14 +10,14 @@ describe('Presence of Logo', () => {
 
 describe('Presence of bar graph test', () => {
   it('Tests that the grafana bar graph is on the page', () => {
-    cy.visit('http://localhost:3003')
+    cy.visit('http://localhost:3001')
     cy.get('iframe[src="http://localhost:3000/d-solo/en2yCsa4k/overview-of-devices?orgId=1&panelId=2&kiosk"]').should('exist');
   })
 })
 
 describe('Health graph button test', () => {
   it('Tests that the health graph button leads to healthgraphs page', () => {
-    cy.visit('http://localhost:3003')
+    cy.visit('http://localhost:3001')
     cy.get('#Health-Graphs-id').click()
     cy.url().should('include', '/healthgraphs')
   })
@@ -25,7 +25,7 @@ describe('Health graph button test', () => {
 
 describe('Health graph button test', () => {
   it('Tests that the health graph button leads to healthgraphs page', () => {
-    cy.visit('http://localhost:3003')
+    cy.visit('http://localhost:3001')
     cy.get('#Health-Graphs-id').click()
     cy.url().should('include', '/healthgraphs')
   })
@@ -35,7 +35,7 @@ describe('Health graph button test', () => {
 
 describe('Grafana Graph test, will break if test page is removed', () => {
   it('Visits the test page', () => {
-    cy.visit('http://localhost:3003/test')
+    cy.visit('http://localhost:3001/test')
     cy.get('iframe[src="http://localhost:3000/d-solo/en2yCsa4k/overview-of-devices?orgId=1&from=1679499012553&to=1679520612553&panelId=2"]').should('exist');
     // ^hardcoded for the homepage graph^
   })
