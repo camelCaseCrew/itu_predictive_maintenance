1fdc3865691adaabf7bcdefd9245a5c383828f5c
Cromski <jakobrhenriksen@outlook.com>
2023-04-20 15:06:49 +0200

    Changed: ports back to the right port

--- cypress/e2e/frontend.cy.js
+++ cypress/e2e/frontend.cy.js
@@ -7,7 +7,7 @@ describe('Presence of bar graph test', () => {
   Object.values(dimensions).map((key, i) => {
     it('Tests that the grafana bar graph is on the page', () => {
       cy.viewport(key.viewportWidth, key.viewportHeight)
-      cy.visit('http://localhost:3003')
+      cy.visit('http://localhost:3001')
       cy.get('iframe[src="http://localhost:3000/d-solo/en2yCsa4k/overview-of-devices?orgId=1&panelId=2&kiosk"]').should('exist');
     })
   })
@@ -19,7 +19,7 @@ describe('Critical button goes to /health_graph', () => {
   Object.values(dimensions).map((key, i) => {
     it('Critical button goes to /health_graph', () => {
       cy.viewport(key.viewportWidth, key.viewportHeight)
-      cy.visit('http://localhost:3003')
+      cy.visit('http://localhost:3001')
       cy.get('[id=Critical-goto-btn-id]').click()
       cy.url().should('include', '/health_graphs')
     })
@@ -30,7 +30,7 @@ describe('Risk button goes to /health_graph', () => {
   Object.values(dimensions).map((key, i) => {
     it('Risk button goes to /health_graph', () => {
       cy.viewport(key.viewportWidth, key.viewportHeight)
-      cy.visit('http://localhost:3003')
+      cy.visit('http://localhost:3001')
       cy.get('[id=Risk-goto-btn-id]').click()
       cy.url().should('include', '/health_graphs')
     })
@@ -41,7 +41,7 @@ describe('Healthy button goes to /health_graph', () => {
   Object.values(dimensions).map((key, i) => {
     it('Healthy button goes to /health_graph', () => {
       cy.viewport(key.viewportWidth, key.viewportHeight)
-      cy.visit('http://localhost:3003')
+      cy.visit('http://localhost:3001')
       cy.get('[id=Healthy-goto-btn-id]').click()
       cy.url().should('include', '/health_graphs')
     })
@@ -52,14 +52,14 @@ describe('Healthy button goes to /health_graph', () => {
 
 describe('Presence of Logo', () => {
   it('Checks for the presence of and "PredictIT"', () => {
-    cy.visit('http://localhost:3003')
+    cy.visit('http://localhost:3001')
     cy.contains('PredictIT') 
   })
 })
 
 describe('Health graph button test', () => {
   it('Tests that the health graph button leads to healthgraphs page', () => {
-    cy.visit('http://localhost:3003')
+    cy.visit('http://localhost:3001')
     cy.get('#Health-Graphs-id').click()
     cy.url().should('include', '/health_graphs')
   })
@@ -69,7 +69,7 @@ describe('Health graph button test', () => {
 
 describe('Grafana Graph test, will break if test page is removed', () => {
   it('Visits the test page', () => {
-    cy.visit('http://localhost:3003/test')
+    cy.visit('http://localhost:3001/test')
     cy.get('iframe[src="http://localhost:3000/d-solo/en2yCsa4k/overview-of-devices?orgId=1&from=1679499012553&to=1679520612553&panelId=2"]').should('exist');
     // ^hardcoded for the homepage graph^
   })
