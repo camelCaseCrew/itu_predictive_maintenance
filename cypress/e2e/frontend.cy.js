63d584690eae735b894b4df0791f77b98b88330e
adamjhr <adam.jh.rose6@gmail.com>
2023-04-20 10:38:25 +0200

    Added: Test for homepage logo

--- cypress/e2e/frontend.cy.js
+++ cypress/e2e/frontend.cy.js
@@ -1,22 +1,31 @@
-describe('Preliminary front end test', () => {
-    it('Visits the frontend website', () => {
-      cy.visit('http://localhost:3001/test') 
-      cy.contains('hello there') // text on the website
-    })
+describe('Presence of Logo', () => {
+  it('Checks for the presence of "Systematic" and "PredictIT"', () => {
+    cy.visit('http://localhost:3001')
+    cy.contains('Systematic')
+    cy.contains('PredictIT') 
   })
+})
+
+describe('Presence of Logo', () => {
+  it('Checks for the presence of "Systematic" and "PredictIT"', () => {
+    cy.visit('http://localhost:3001')
+    cy.contains('Systematic')
+    cy.contains('PredictIT')
+  })
+})
 
 describe('Grafana Graph test, will break if test page is removed', () => {
-    it('Visits the test page', () => {
-      cy.visit('http://localhost:3001/test')
-      cy.get('iframe[src="http://localhost:3000/d-solo/en2yCsa4k/overview-of-devices?orgId=1&from=1679499012553&to=1679520612553&panelId=2"]').should('exist'); 
-      // ^hardcoded for the homepage graph^
-    })
+  it('Visits the test page', () => {
+    cy.visit('http://localhost:3001/test')
+    cy.get('iframe[src="http://localhost:3000/d-solo/en2yCsa4k/overview-of-devices?orgId=1&from=1679499012553&to=1679520612553&panelId=2"]').should('exist');
+    // ^hardcoded for the homepage graph^
   })
+})
 
-  describe('Navbar test', () => {
-    it('Ensures functionality of navbar button', () => {
-      cy.visit('http://localhost:3001')
-      cy.get('#Health-Graphs-id').click()
-      cy.url().should('include', '/healthgraphs')
-    })
+describe('Navbar test', () => {
+  it('Ensures functionality of navbar button', () => {
+    cy.visit('http://localhost:3001')
+    cy.get('#Health-Graphs-id').click()
+    cy.url().should('include', '/healthgraphs')
   })
+})
