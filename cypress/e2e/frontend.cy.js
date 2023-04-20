654740a7647ce772925c05b0a8aff1ad371c2732
Cromski <jakobrhenriksen@outlook.com>
2023-04-20 15:06:08 +0200

    Added: Tests for responsiveness of the home page

--- cypress/e2e/frontend.cy.js
+++ cypress/e2e/frontend.cy.js
@@ -1,35 +1,50 @@
+import 'cypress-iframe';
+
+const dimensions = require('../dimensions.js')
 // Home page tests
 
 describe('Presence of bar graph test', () => {
-  it('Tests that the grafana bar graph is on the page', () => {
-    cy.visit('http://localhost:3001')
-    cy.get('iframe[src="http://localhost:3000/d-solo/en2yCsa4k/overview-of-devices?orgId=1&panelId=2&kiosk"]').should('exist');
+  Object.values(dimensions).map((key, i) => {
+    it('Tests that the grafana bar graph is on the page', () => {
+      cy.viewport(key.viewportWidth, key.viewportHeight)
+      cy.visit('http://localhost:3003')
+      cy.get('iframe[src="http://localhost:3000/d-solo/en2yCsa4k/overview-of-devices?orgId=1&panelId=2&kiosk"]').should('exist');
+    })
   })
 })
 
 // Home page - Overview buttons
 
 describe('Critical button goes to /health_graph', () => {
-  it('Critical button goes to /health_graph', () => {
-    cy.visit('http://localhost:3003')
-    cy.get('[id=Critical-goto-btn-id]').click()
-    cy.url().should('include', '/health_graphs')
+  Object.values(dimensions).map((key, i) => {
+    it('Critical button goes to /health_graph', () => {
+      cy.viewport(key.viewportWidth, key.viewportHeight)
+      cy.visit('http://localhost:3003')
+      cy.get('[id=Critical-goto-btn-id]').click()
+      cy.url().should('include', '/health_graphs')
+    })
   })
 })
 
 describe('Risk button goes to /health_graph', () => {
-  it('Risk button goes to /health_graph', () => {
-    cy.visit('http://localhost:3003')
-    cy.get('[id=Risk-goto-btn-id]').click()
-    cy.url().should('include', '/health_graphs')
+  Object.values(dimensions).map((key, i) => {
+    it('Risk button goes to /health_graph', () => {
+      cy.viewport(key.viewportWidth, key.viewportHeight)
+      cy.visit('http://localhost:3003')
+      cy.get('[id=Risk-goto-btn-id]').click()
+      cy.url().should('include', '/health_graphs')
+    })
   })
 })
 
 describe('Healthy button goes to /health_graph', () => {
-  it('Healthy button goes to /health_graph', () => {
-    cy.visit('http://localhost:3003')
-    cy.get('[id=Healthy-goto-btn-id]').click()
-    cy.url().should('include', '/health_graphs')
+  Object.values(dimensions).map((key, i) => {
+    it('Healthy button goes to /health_graph', () => {
+      cy.viewport(key.viewportWidth, key.viewportHeight)
+      cy.visit('http://localhost:3003')
+      cy.get('[id=Healthy-goto-btn-id]').click()
+      cy.url().should('include', '/health_graphs')
+    })
   })
 })
 
@@ -44,7 +59,7 @@ describe('Presence of Logo', () => {
 
 describe('Health graph button test', () => {
   it('Tests that the health graph button leads to healthgraphs page', () => {
-    cy.visit('http://localhost:3001')
+    cy.visit('http://localhost:3003')
     cy.get('#Health-Graphs-id').click()
     cy.url().should('include', '/health_graphs')
   })
@@ -54,7 +69,7 @@ describe('Health graph button test', () => {
 
 describe('Grafana Graph test, will break if test page is removed', () => {
   it('Visits the test page', () => {
-    cy.visit('http://localhost:3001/test')
+    cy.visit('http://localhost:3003/test')
     cy.get('iframe[src="http://localhost:3000/d-solo/en2yCsa4k/overview-of-devices?orgId=1&from=1679499012553&to=1679520612553&panelId=2"]').should('exist');
     // ^hardcoded for the homepage graph^
   })
