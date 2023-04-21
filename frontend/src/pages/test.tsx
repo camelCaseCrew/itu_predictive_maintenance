5290cbc497c8fb60f0d7904c96c015d35f24c91f
eikbo <eikb@itu.dk>
2023-04-21 12:53:23 +0200

    Added: Primereact CSS, Changed: Test to test Log components

--- frontend/src/pages/test.tsx
+++ frontend/src/pages/test.tsx
@@ -1,4 +1,4 @@
-
+import LogData from '@/components/LogData'
 export default function test() {
   return (
     <>
@@ -6,6 +6,7 @@ export default function test() {
       <div>
       <iframe src="http://localhost:3000/d-solo/en2yCsa4k/overview-of-devices?orgId=1&from=1679499012553&to=1679520612553&panelId=2"></iframe>
       </div>
+      <LogData />
 
     </>
   )
