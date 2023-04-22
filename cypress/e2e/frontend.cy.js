d0c4e446db427cc0f4b3b40b4c113d42b5d58543
Dayo <90214655+DanyalYor@users.noreply.github.com>
2023-04-22 19:01:52 +0200

    Revert "Changed: fixed time test to work with new filters"
    This reverts commit 83bbf30f7a7f388b70cb21084c49b2aacde62838.

--- cypress/e2e/frontend.cy.js
+++ cypress/e2e/frontend.cy.js
@@ -2,13 +2,13 @@ import 'cypress-iframe';
 
 describe('Preliminary front end test', () => {
   it('Visits the frontend website', () => {
-    cy.visit('http://localhost:3003/test') 
+    cy.visit('http://localhost:3001/test') 
   })
 })
 
 describe('Grafana Graph test, will break if test page is removed', () => {
   it('Visits the test page', () => {
-    cy.visit('http://localhost:3003/test')
+    cy.visit('http://localhost:3001/test')
     cy.get('iframe[src="http://localhost:3000/d/enayayaya/overview-of-devices-copy?orgId=1&refresh=60s&kiosk"]').should('exist'); 
     // ^hardcoded for the homepage graph^
   })
@@ -16,26 +16,33 @@ describe('Grafana Graph test, will break if test page is removed', () => {
 
 describe('Filtering buttons works', () => {
   it('Checks that filtering buttons have been created', () => {
-    cy.visit('http://localhost:3003/test')
+    cy.visit('http://localhost:3001/test')
 
-    // filter by time
-    cy.get('input[id="timeFilter"]').parent().parent().click()
-    cy.get('ul[class="p-dropdown-items"]').contains("Last 1 hour").click()
-    cy.get('iframe').invoke('attr', 'src').should('contain', 'from=now-1h&to=now')
+    // check filtering buttons exist
+    cy.get('select[name="healthFilter"]').should('exist')
+    cy.get('input[name="serialNumberFilter"]').should('exist')
+    cy.get('input[name="modelFilter"').should('exist')
+    cy.get('input[name="timeFilter"').should('exist')
 
-    /*
+    // filter by risk group
+    cy.get('select[name="healthFilter"]').select('Critical')
+    cy.get('iframe').invoke('attr', 'src').should('contain', 'var-risk_group=critical')
+
     // filter by search
-    cy.get('input[id="serialNumberFilter"]').type('aoao{enter}')
+    cy.get('input[name="serialNumberFilter"]').type('aoao{enter}')
     cy.get('iframe').invoke('attr', 'src').should('contain', 'var-serial_number=aoao')
-
+    /*
     cy.frameLoaded()
     const device = cy.iframe().find('div[class="grafana-app"]', {timeout: 20000 })
     device.find('div[class="panel-title"]').find('h2').should('contain', 'aoao')
     */
+    
+    // filter by time
+    cy.get('input[name="timeFilter"]').parent().click().contains('Last 1 hour').click()
+    cy.get('iframe').invoke('attr', 'src').should('contain', 'from=now-1h&to=now')
   })
 })
 
-/*
 describe('Navbar test', () => {
   it('Ensures functionality of navbar button', () => {
     cy.visit('http://localhost:3001')
@@ -43,4 +50,3 @@ describe('Navbar test', () => {
     cy.url().should('include', '/healthgraphs')
   })
 })
-*/
