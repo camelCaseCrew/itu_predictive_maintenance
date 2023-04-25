35c7355a0964cfbdf680ddd677d351874cfb3d34
jfho <jfho@itu.dk>
2023-04-25 21:32:46 +0200

    Changed: now uses 3 iframes on homepage

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -5,8 +5,11 @@ export default function App() {
       <h3 className=" font-bold text-gray-700 text-center"> Home Page</h3>
 
       <div className="flex row justify-center gap-4 flex-wrap h-screen w-full">
-        {/*This source is a link to the grafana dashboard with uid=en2yCsa4k in kiosk mode*/}
-        <iframe id="test" className=" w-full h-full" loading="lazy" src={`http://localhost:3000/d/en2yCsa4k/overview-of-devices?orgId=1&refresh=60s&kiosk`}></iframe>  
+        <iframe src="http://localhost:3000/d-solo/en2yCsa4k/overview-of-devices?orgId=1&panelId=2" width="450" height="200"></iframe>
+
+        <iframe src="http://localhost:3000/d-solo/en2yCsa4k/overview-of-devices?orgId=1&panelId=3" width="450" height="200"></iframe>
+
+        <iframe src="http://localhost:3000/d-solo/en2yCsa4k/overview-of-devices?orgId=1&panelId=4" width="450" height="200"></iframe>
       </div>
     </> 
   )
