4cb652414a0652ef621bc3bce54d5aceda9c111a
Cromski <jakobrhenriksen@outlook.com>
2023-04-26 00:12:02 +0200

    Added: styling to footer, it's now stuck at the bottom, text in the center

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,8 +1,8 @@
 
 
 const Footer = () => (
-    <div className=" w-screen">
-        <h1>Systematic x ITU</h1>
+    <div className=" w-full -ml-4 absolute justify-center text-center bottom-0 mt-auto bg-background">
+        <h1 className=" text-text">Systematic <span className=" text-xs">&#10005;</span> ITU</h1>
     </div>
 )
 
