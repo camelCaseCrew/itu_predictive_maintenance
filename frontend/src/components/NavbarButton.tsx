e62ee47b66e6a39839ff48d4360c24c728439ad1
adamjhr <adam.jh.rose6@gmail.com>
2023-04-26 23:10:43 +0200

    Changed: Moved id from div-tag to Link component

--- frontend/src/components/NavbarButton.tsx
+++ frontend/src/components/NavbarButton.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import Link from 'next/link';
 
 function NavbarButton(props: { title: string, href: string }) {
-    return <div id={props.title + "-id"} className="bg-component2 text-text w-48 h-10 md:gap-x-4 md:hover:scale-105 transition duration-300 shadow-2xl mr-2">
-        <Link href={props.href} className='h-full w-full flex justify-center place-items-center text-xs md:text-base'>
+    return <div className="bg-component2 text-text w-48 h-10 md:gap-x-4 md:hover:scale-105 transition duration-300 shadow-2xl mr-2">
+        <Link id={props.title + "-id"} href={props.href} className='h-full w-full flex justify-center place-items-center text-xs md:text-base'>
             {props.title}
         </Link>
     </div>
