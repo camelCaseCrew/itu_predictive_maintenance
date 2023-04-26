8933854aee9df2afac3b033f91f85100b153366b
eikbo <eikb@itu.dk>
2023-04-26 17:13:32 +0200

    Added: Date is now UTC pog

--- frontend/src/components/LogDataComponent.tsx
+++ frontend/src/components/LogDataComponent.tsx
@@ -17,7 +17,7 @@ interface LogDataComponentProps {
 const LogDataComponent: React.FC<LogDataComponentProps> = ({ metricData }) => {
     const [style, updateStyle] = useState("")
 
-
+    const date = new Date (metricData.date * 1000)
     useEffect(() => {
         const percentage = parseFloat(metricData.percentage) * 100
         if (percentage > 50) {
@@ -35,7 +35,7 @@ const LogDataComponent: React.FC<LogDataComponentProps> = ({ metricData }) => {
     <div className="flex place-content-between m-2 p-2 items-center">
     <div className={style + " flex place-content-between items-center grow p-1 mr-16 pr-8 pl-8"}>
       <p className="text-white inline-block p-4 opacity-100 align-middle w-48 text-center">{ metricData.serial_number }</p>
-      <p className="text-white inline-block p-4 opacity-100 align-middle w-48 text-center">{ metricData.date }</p>
+      <p className="text-white inline-block p-4 opacity-100 align-middle w-48 text-center">{ date.toUTCString() }</p>
       <p className="text-white inline-block p-4 opacity-100 align-middle w-48 text-center">{ metricData.type }</p>
       <p className="text-white inline-block p-4 opacity-100 align-middle w-48 text-center">{ metricData.percentage }</p>
       
