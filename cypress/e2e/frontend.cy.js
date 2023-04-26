2bc7ace9798284ef5d73c65ec7964587e48377ea
eikbo <eikb@itu.dk>
2023-04-26 17:52:06 +0200

    Tests

--- cypress/e2e/frontend.cy.js
+++ cypress/e2e/frontend.cy.js
@@ -13,10 +13,30 @@ describe('Grafana Graph test, will break if test page is removed', () => {
     })
   })
 
-  describe('Navbar test', () => {
+  describe('Navbar button for health graphs works', () => {
     it('Ensures functionality of navbar button', () => {
       cy.visit('http://localhost:3001')
       cy.get('#Health-Graphs-id').click()
       cy.url().should('include', '/healthgraphs')
     })
   })
+
+  describe('Navbar button for history works', () => {
+    it('Ensures functionality of navbar button', () => {
+      cy.visit('http://localhost:3001')
+      cy.get('#History-id').click()
+      cy.url().should('include', '/history')
+    })
+  })
+
+  describe('LogData component is creating functional elements', () => {
+    Object.values(dimensions).map((key, i) => {
+      it('Graph goes to /health_graph', () => {
+        cy.viewport(key.viewportWidth, key.viewportHeight)
+        cy.visit('http://localhost:3001/history')
+        cy.get('[id=Critical-goto-graph-id]').click()
+        cy.url().should('include', '/health_graphs')
+      })
+    })
+  })
+
