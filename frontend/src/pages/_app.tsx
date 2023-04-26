95a3345fc3735c5abc32bbfb5f776a37d17f6d70
Danyal Yorulmaz <skoledanyal@gmail.com>
2023-04-26 18:11:00 +0200

    Added: BackButton to the interface

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import '@/styles/globals.css'
 import Navbar from '@/components/Navbar'
 import type { AppProps } from 'next/app'
+import BackButton from '@/components/BackButton'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <div id="main" className='bg-background w-screen h-screen p-4'>
       <Navbar />
+      <BackButton></BackButton>
       <Component {...pageProps} />
     </div>
 }
