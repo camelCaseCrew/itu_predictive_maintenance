84fa938f69376a30c4f08f9373c55d4de555b986
Danyal Yorulmaz <skoledanyal@gmail.com>
2023-04-26 18:23:16 +0200

    Added: ID to the backbutton component for use in e2e test

--- frontend/src/components/BackButton.tsx
+++ frontend/src/components/BackButton.tsx
@@ -4,7 +4,7 @@ import { ArrowLeftIcon } from '@heroicons/react/solid';
 function BackButton() {
     const router = useRouter();
     return (
-        <div className='h-10 w-10 rounded md:ml-8 md:mt-12 mt-4 sticky top-12 text-white bg-component1 flex justify-center place-items-center hover:cursor-pointer hover:scale-110 transition duration-300 shadow-2xl md:h-16 md:w-16' onClick={() => router.back()}>
+        <div id="Back-Button" className='h-10 w-10 rounded md:ml-8 md:mt-12 mt-4 sticky top-12 text-white bg-component1 flex justify-center place-items-center hover:cursor-pointer hover:scale-110 transition duration-300 shadow-2xl md:h-16 md:w-16' onClick={() => router.back()}>
             <ArrowLeftIcon className='h-6 w-6 md:h-10 md:w-10'></ArrowLeftIcon>
         </div>
     )
