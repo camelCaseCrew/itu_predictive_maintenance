1246455e6c3fb8c4459596d1075bf2bd415aa68f
Cromski <jakobrhenriksen@outlook.com>
2023-04-26 10:58:16 +0200

    Changed: Styling on Footer, to have the same style as the NavBar, And updated the text from the abbreviation of ITU to full name

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,8 +1,8 @@
 
 
 const Footer = () => (
-    <div className=" w-full -ml-4 absolute justify-center text-center bottom-0 mt-auto bg-background">
-        <h1 className=" text-text">Systematic <span className=" text-xs">&#10005;</span> ITU</h1>
+    <div className=" w-full -ml-4 h-11 absolute text-center bottom-0 mt-auto bg-component1 border-background border-x-[16px] border-solid border-b-8">
+        <h1 className=" leading-9 text-text">Systematic <span className=" text-xs">&#10005;</span> IT University of Copenhagen</h1>
     </div>
 )
 
