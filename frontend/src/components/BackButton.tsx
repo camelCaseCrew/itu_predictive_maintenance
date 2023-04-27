2103a8c708ed9833a185a67bea11787bdd84f4c6
adamjhr <adam.jh.rose6@gmail.com>
2023-04-27 10:24:40 +0200

    Removed: Sticky styling

--- frontend/src/components/BackButton.tsx
+++ frontend/src/components/BackButton.tsx
@@ -4,7 +4,7 @@ import { ArrowLeftIcon } from '@heroicons/react/solid';
 function BackButton() {
     const router = useRouter();
     return (
-        <div id="Back-Button" className='h-10 w-10 rounded sticky top-12 ml-4 mr-2 mt-4 text-text bg-component1 flex justify-center place-items-center hover:cursor-pointer hover:scale-110 transition duration-300 shadow-2xl md:h-16 md:w-16' onClick={() => router.back()}>
+        <div id="Back-Button" className='h-10 w-10 rounded top-12 ml-4 mr-2 mt-4 text-text bg-component1 flex justify-center place-items-center hover:cursor-pointer hover:scale-110 transition duration-300 shadow-2xl md:h-16 md:w-16' onClick={() => router.back()}>
             <ArrowLeftIcon className='h-6 w-6 md:h-10 md:w-10'></ArrowLeftIcon>
         </div>
     )
