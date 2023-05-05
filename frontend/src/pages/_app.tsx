3026c9dfd171893fa6c823521edb0c3d2b7ec9bc
eikbo <eikb@itu.dk>
2023-05-05 22:56:14 +0200

    Added: Wrapped entire app in globalContext

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import '@/styles/globals.css'
 import Navbar from '@/components/Navbar'
+import { GlobalContextProvider } from "@/context/global";
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <div id="main" className='bg-background w-screen h-screen p-4'>
+    
+    <GlobalContextProvider>
       <Navbar />
       <Component {...pageProps} />
+    </GlobalContextProvider>
     </div>
 }
