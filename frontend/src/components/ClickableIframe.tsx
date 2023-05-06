5cdc3353a225862136d617df200c6235446ffee4
eikbo <eikb@itu.dk>
2023-05-06 11:47:38 +0200

    Added: iframes changes context

--- frontend/src/components/ClickableIframe.tsx
+++ frontend/src/components/ClickableIframe.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import { useGlobal } from '@/context/global';
 
 const ClickableIframe = (props: { src: string, redirectUrl: string, Id: string }) => {
+  const { filterInt, changeFilter } = useGlobal();
   const router = useRouter();
 
   const handleClick = () => {
+    if (props.Id == "Healthy-goto-graph-id") {
+      changeFilter(1)
+    } else if (props.Id == "Risk-goto-graph-id") {
+      changeFilter(2)
+    } else if (props.Id == "Critical-goto-graph-id") {
+      changeFilter(3)
+    }
     router.push(props.redirectUrl);
   };
 
