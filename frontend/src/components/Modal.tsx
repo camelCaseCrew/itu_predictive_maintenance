5e08b83c2e43951bc5b82582c9acc4c06b0b3078
Cromski <jakobrhenriksen@outlook.com>
2023-05-09 22:38:25 +0200

    Changed: regex that checks for email validation

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -4,7 +4,7 @@ import { Dialog, Transition } from '@headlessui/react'
 export default function Modal() {
   
   function validateEmail(email: string) {
-    return String(email).toLowerCase().match(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/)
+    return String(email).toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
   }
 
   const [email, setEmail] = useState('')
