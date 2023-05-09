65ea0a6f06707e9697937935ffce02fad98f68f5
Danyal Yorulmaz <skoledanyal@gmail.com>
2023-05-09 21:26:01 +0200

    Changed: Newly added Modal component now gets shown when Subscribe is clicked

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import NavbarButton from './NavbarButton';
+import Modal from './Modal';
 import Logo from './Logo';
 
 
@@ -8,18 +9,16 @@ const Navbar = () => {
     const [showModal, updateShowModal] = useState<Boolean>(false)
 
     return (
-        <div id="navbar" className=" relative bg-component1 text-text rounded h-20 flex justify-start place-items-center md:gap-x-8">
+        <div id="navbar" className="relative bg-component1 text-text rounded h-20 flex justify-start place-items-center md:gap-x-8">
             <Logo></Logo>
             <NavbarButton title="Health-Graphs" href="/health_graphs"></NavbarButton>
             <NavbarButton title="History" href="/history"></NavbarButton>
             <div className="bg-component2 text-text text-center leading-10 cursor-pointer select-none w-48 h-10 md:gap-x-4 md:hover:scale-105 transition duration-300 shadow-2xl mr-2"
                  onClick={() => updateShowModal(prevValue => !prevValue)}>
-                Notifications
+                Subscribe
             </div>
             {showModal &&
-                <div className=' flex  w-56 h-80 bg-component2'>
-                        asdf
-                </div>
+                <Modal></Modal>
             }
         </div>
     )
