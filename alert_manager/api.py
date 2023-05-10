e36f8cbee3eed67f4cda9b57bc02aa19dfb3ae69
Danyal Yorulmaz <skoledanyal@gmail.com>
2023-05-10 20:02:07 +0200

    Added: DELETE endpoint for alertmanager API

--- alert_manager/api.py
+++ alert_manager/api.py
@@ -8,7 +8,7 @@ app = flask.Flask(__name__)
 @app.route("/update/<email>", methods=["PUT"])
 def update(email):
     emails.append(email)
-    print("emails updated: ", emails)
+    print("emails added: ", email)
 
     with open("alertmanager.yml", "r") as f:
         data = yaml.load(f, Loader=yaml.FullLoader)
@@ -20,6 +20,31 @@ def update(email):
     print("update config file")
     return {
         "message": "added email to list",
+        "code": 200
+    }
+
+@app.route("/remove/<email>", methods=["DELETE"])
+def update(email):
+    if not emails.__contains__(email):
+        print("This email is not already subscribed to alerts.")
+        return {
+            "message": "Bad Request: This email is not already subscribed to alerts.",
+            "code": 400
+        }
+    
+    emails.pop(email)
+    print("email removed: ", email)
+
+    with open("alertmanager.yml", "r") as f:
+        data = yaml.load(f, Loader=yaml.FullLoader)
+    data.get("receivers")[0].get("email_configs")[0]["to"] = ', '.join(emails)
+    print("data variable: ", data)
+
+    with open("alertmanager.yml", "w") as f:
+        yaml.dump(data, f)
+    print("update config file")
+    return {
+        "message": "Removed email from list",
         "code": 200
     }
 
