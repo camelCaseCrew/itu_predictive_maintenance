db98c1e6d69353123123fb436ab652fae9543711
Cromski <jakobrhenriksen@outlook.com>
2023-05-10 10:31:40 +0200

    Changed: ports to use our docker FE ports

--- cypress/e2e/navbar.cy.js
+++ cypress/e2e/navbar.cy.js
@@ -4,7 +4,7 @@ describe('Subscribe button gives pop up', () => {
     Object.values(dimensions).map((key, i) => {
       it('Subscribe button gives pop up', () => {
         cy.viewport(key.viewportWidth, key.viewportHeight)
-        cy.visit('http://localhost:3003')
+        cy.visit('http://localhost:3001')
         cy.get('#subscribe-id').click()
         cy.contains('Subscribe to Email notifications:')
         
@@ -18,7 +18,7 @@ describe('Invalid email', () => {
     Object.values(dimensions).map((key, i) => {
       it('Shows error message when inserted invalid email', () => {
         cy.viewport(key.viewportWidth, key.viewportHeight)
-        cy.visit('http://localhost:3003')
+        cy.visit('http://localhost:3001')
         cy.get('#subscribe-id').click()
         cy.get('#textInput-id').type("notAnEmail")
         cy.contains("Please enter a valid Email address")
@@ -31,7 +31,7 @@ describe('Valid email', () => {
     Object.values(dimensions).map((key, i) => {
         it('Shows error message when inserted invalid email', () => {
         cy.viewport(key.viewportWidth, key.viewportHeight)
-        cy.visit('http://localhost:3003')
+        cy.visit('http://localhost:3001')
         cy.get('#subscribe-id').click()
         cy.get('#textInput-id').type("a@cool.email")
         cy.contains("Please enter a valid Email address").should("not.exist")
