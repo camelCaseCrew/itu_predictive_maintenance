23b60f23012c5cc83eda2dd6ca560a1c6993e878
adamjhr <adam.jh.rose6@gmail.com>
2023-05-10 21:17:17 +0200

    Added: Tests for emails

---
+++ cypress/e2e/alerting.cy.js
@@ -0,0 +1,36 @@
+import { uniqueId } from "cypress/types/lodash"
+
+describe('Alert activated', () => {
+    it('Checks that prometheus alerts have been activated', () => {
+        cy.request('http://localhost:9090/api/v1/alerts')
+        .its('status')
+        .should('equal', 'success')
+    })
+})
+
+describe('Alert activated', () => {
+    it('Checks that prometheus alerts have been activated', () => {
+        cy.wait(10000)
+        cy.request('https://api.mail.tm/domains').then(( response ) => {
+            const accountName = uniqueId() + '@' + response.body[0]['domain']
+            const password = "predictit123"
+
+            cy.request('POST', 'https://api.mail.tm/accounts', { address: accountName, password: password})
+            cy.request('http://localhost:5000/update/'+accountName)
+            cy.request('POST', 'https://api.mail.tm/token', { address: accountName, password: password}).then(( response ) => {
+                const token = response.body.token
+                const id = response.body.id
+
+                cy.request({
+                    url: 'https://api.mail.tm/messages',
+                    auth: token
+                }).then((response) => {
+                    response.body["hydra:totalItems"]
+                })
+
+                cy.request('DELETE', 'https://api.mail.tm/accounts/' + id)
+            })
+
+        })
+    })
+})
