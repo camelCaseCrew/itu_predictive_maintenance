7d5f7b112d9a040c900df8810ebcadff4accd766
Cromski <jakobrhenriksen@outlook.com>
2023-05-10 10:30:13 +0200

    Changed: styling on button, and text from Subscribe to Get Alerts

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -14,9 +14,9 @@ const Navbar = () => {
             <Logo></Logo>
             <NavbarButton title="Health-Graphs" href="/health_graphs"></NavbarButton>
             <NavbarButton title="History" href="/history"></NavbarButton>
-            <div id='subscribe-id' className="bg-component2 text-text text-center leading-10 cursor-pointer select-none w-48 h-10 md:gap-x-4 md:hover:scale-105 transition duration-300 shadow-2xl mr-2"
+            <div id='subscribe-id' className="bg-component1 text-text text-center ml-auto mr-10 leading-10 cursor-pointer select-none rounded-full outline outline-1 w-28 h-10 md:gap-x-4 md:hover:scale-105 transition duration-300 shadow-2xl"
                  onClick={() => modalContext.updateShowModal(!modalContext.showModal)}>
-                Subscribe
+                Get Alerts
             </div>
             {modalContext.showModal &&
                 <Modal></Modal>
