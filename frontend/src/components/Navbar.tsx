00fb576bf3a9220138c8d79095e000cc40bb5e02
Cromski <jakobrhenriksen@outlook.com>
2023-05-10 11:44:16 +0200

    Added: icons to show on smaller screens

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,20 +3,23 @@ import NavbarButton from './NavbarButton';
 import Modal from './Modal';
 import Logo from './Logo';
 import ModalContext from '@/context/ModalContext';
+import { ClockIcon, PresentationChartLineIcon, BellIcon } from '@heroicons/react/outline';
+
 
 
 const Navbar = () => {
 
-    const modalContext = useContext(ModalContext)   
+    const modalContext = useContext(ModalContext)
 
     return (
         <div id="navbar" className="relative bg-component1 text-text rounded h-20 flex justify-start place-items-center md:gap-x-8">
             <Logo></Logo>
-            <NavbarButton title="Health-Graphs" href="/health_graphs"></NavbarButton>
-            <NavbarButton title="History" href="/history"></NavbarButton>
+            <NavbarButton title="Health-Graphs" href="/health_graphs"><PresentationChartLineIcon/></NavbarButton>
+            <NavbarButton title="History" href="/history"><ClockIcon/></NavbarButton>
             <div id='subscribe-id' className="bg-component1 text-text text-center ml-auto mr-10 leading-10 cursor-pointer select-none rounded-full outline outline-1 w-28 h-10 md:gap-x-4 md:hover:scale-105 transition duration-300 shadow-2xl"
                  onClick={() => modalContext.updateShowModal(!modalContext.showModal)}>
-                Get Alerts
+                <div className=' w-[30px] mx-auto mt-1 sm:hidden'><BellIcon /></div>
+                <div className=' hidden sm:block'>Get Alerts</div>  
             </div>
             {modalContext.showModal &&
                 <Modal></Modal>
