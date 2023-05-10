ec8415176831f404b8726944f756f7c62c4ac9d0
adamjhr <adam.jh.rose6@gmail.com>
2023-05-10 22:51:48 +0200

    Changed: using uuidv4

--- cypress/e2e/alerting.cy.js
+++ cypress/e2e/alerting.cy.js
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from 'uuid';
+
 describe('Alert activated', () => {
     it('Checks that prometheus alerts have been activated', () => {
         cy.request('http://localhost:9090/api/v1/alerts')
@@ -10,7 +12,7 @@ describe('Alert activated', () => {
     it('Checks that prometheus alerts have been activated', () => {
         cy.wait(10000)
         cy.request('https://api.mail.tm/domains').then(( response ) => {
-            const accountName = Cypress._.uniqueId() + '@' + response.body["hydra:member"][0]["domain"]
+            const accountName = uuidv4() + '@' + response.body["hydra:member"][0]["domain"]
             const password = "predictit123"
 
             cy.request('POST', 'https://api.mail.tm/accounts', { address: accountName, password: password})
