1d5938953b631886f84e609a52ea8709d32662ea
adamjhr <adam.jh.rose6@gmail.com>
2023-05-10 13:29:32 +0200

    Changed: Comments, cleanup

--- frontend/src/components/LogData.tsx
+++ frontend/src/components/LogData.tsx
@@ -31,7 +31,7 @@ interface TypeList {
   types: string[]
 }
 
-const PrometheusData: React.FC<TypeList>= ({ types }) => {
+const PrometheusData: React.FC<TypeList> = ({ types }) => {
   const [data, setData] = useState<FlattenedData[]>([]); // Data from prometheus
   const [filteredData, setFilteredData] = useState<FlattenedData[]>([]); // Filtered data
   const [hasMore, setHasMore] = useState(true)
@@ -45,7 +45,7 @@ const PrometheusData: React.FC<TypeList>= ({ types }) => {
       const startTimestamp = Math.floor((Date.now() / 1000) - 60 * 60);
       const endTimestamp = Math.floor(Date.now() / 1000);
       const stepDuration = 15;
-      const query = `device_health{group="healthy"}`;
+      const query = `device_health`;
 
       const response = await axios.get('http://localhost:9090/api/v1/query_range', {
         params: {
@@ -59,57 +59,34 @@ const PrometheusData: React.FC<TypeList>= ({ types }) => {
       if (response.data.status === 'success') {
 
         // Yes i know this is ugly. It reformats data into being of type FlattenedData.
-        const flattenedData: FlattenedData[] = 
+        const flattenedData: FlattenedData[] =
           response.data.data.result.map(
             (device: MetricData) => (device.values.map(
               (datapoint: DataPoint) => ({ date: datapoint[0], percentage: datapoint[1], type: device.metric["device_type"], serial_number: device.metric["serial_number"] }))))
-              .flat(1) // This final method takes a list of lists: [['a', 'b'], ['c']] and flattens it: ['a', 'b', 'c']
-              
-              setData(flattenedData)
-              setFilteredData(flattenedData)
-              setHasMore(true)
-              loadMoreData()
-              setDataIsLoaded(true)
+            .flat(1) // This final method takes a list of lists: [['a', 'b'], ['c']] and flattens it: ['a', 'b', 'c']
+
+        setData(flattenedData)
+        setFilteredData(flattenedData)
+        setHasMore(true)
+        loadMoreData()
+        setDataIsLoaded(true)
       }
     } catch (error) {
       console.error('Error fetching data from Prometheus:', error);
     }
   }
 
-
-  useEffect(() => { // This hook is only run once when the page is loaded.
-    fetchData()
-    if (types.length === 0) {
-      setFilteredData(data)
-    } else {
-      setFilteredData(data.filter(( dataPoint ) => { return types.includes(dataPoint.type) }))
-    }
-  }, []);
-
-  useEffect(() => {
+  function applyFilters() {
     if (dataIsLoaded) {
       var filtered
       if (types.length === 0) {
         setFilteredData(data)
       } else {
-        filtered = data.filter(( dataPoint ) => { return types.includes(dataPoint.type) })
+        filtered = data.filter((dataPoint) => { return types.includes(dataPoint.type) }) // Apply the types filter
         setFilteredData(filtered)
       }
     }
-  }, [data])
-
-  useEffect(() => {
-    if (dataIsLoaded) {
-      var filtered
-      if (types.length === 0) {
-        setFilteredData(data)
-      } else {
-        filtered = data.filter(( dataPoint ) => { return types.includes(dataPoint.type) })
-        setFilteredData(filtered)
-      }
-      reloadDisplayedData()
-    }
-  }, [types])
+  }
 
   function reloadDisplayedData() {
     setDisplayedData([])
@@ -117,16 +94,12 @@ const PrometheusData: React.FC<TypeList>= ({ types }) => {
     setHasMore(true)
   }
 
-  useEffect(() => {
-    loadMoreData()
-  }, [displayedData])
-
-  // Called every time one scrolls to the bottom.
+  // Called every time one scrolls to the bottom or 'displayedData' is updated
   // Data is taken from 'data' to 'displayedData', and thereafter is rendered
   function loadMoreData() {
     var newDisplayedData = displayedData
 
-    for (let i = itemsAmount; i < itemsAmount + 10; i++) {
+    for (let i = itemsAmount; i < itemsAmount + 10; i++) { // Try loading 10 new elements from filteredData into displayedData
       setItemsAmount(i)
 
       if (i >= filteredData.length) { // If there is no data left which isn't already displayed
@@ -136,33 +109,49 @@ const PrometheusData: React.FC<TypeList>= ({ types }) => {
         setHasMore(true)
       }
 
-      newDisplayedData.push(filteredData[i])
+      newDisplayedData.push(filteredData[i]) // Push from filteredData to displayedData
     }
-
-    setDisplayedData(newDisplayedData)
+    setDisplayedData(newDisplayedData) // In theory this creates an infinite loop, but it works nonetheless......
   }
 
 
+  useEffect(() => { // This hook is only run once when the page is loaded
+    fetchData()
+  }, []);
+
+  useEffect(() => {
+    applyFilters() // When data-state changes, reapply filters
+  }, [data])
+
+  useEffect(() => { // When filters change, apply filters and reload all displayed data
+    applyFilters()
+    reloadDisplayedData()
+  }, [types])
+
+  useEffect(() => { // When displayed data changes, more data should be loaded
+    loadMoreData()
+  }, [displayedData])
+
   return (
     <div id="parent" className="overflow-auto h-full">
-      <InfiniteScroll 
-        dataLength={displayedData.length} 
+      <InfiniteScroll
+        dataLength={displayedData.length}
         next={loadMoreData}
         hasMore={hasMore}
         scrollableTarget="parent"
         loader={<h4 className='text-text text-center'>Loading...</h4>}
-          endMessage={
-            <p className='text-text text-center'>
-              <b>No more data</b>
-            </p>
-          }
-        >
+        endMessage={
+          <p className='text-text text-center'>
+            <b>No more data</b>
+          </p>
+        }
+      >
         {displayedData.map((metricData, index) => (
           <LogDataComponent key={index} metricData={metricData} />
         ))}
       </InfiniteScroll>
 
-      
+
     </div>
   );
 };
