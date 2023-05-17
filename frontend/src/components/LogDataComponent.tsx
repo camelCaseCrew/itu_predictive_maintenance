c02d5a5dc1dbe3a1201918a663081500a5071f7f
eikbo <eikb@itu.dk>
2023-05-17 15:25:30 +0200

    First experiment

--- frontend/src/components/LogDataComponent.tsx
+++ frontend/src/components/LogDataComponent.tsx
@@ -37,10 +37,10 @@ const LogDataComponent: React.FC<LogDataComponentProps> = ({ metricData }) => {
   return (
     <li className="flex place-content-between m-2 p-2 items-center" data-testid="container">
     <div className={style + " flex place-content-between items-center grow p-1 mr-16 pr-8 pl-8"} data-testid="inner-container">
-    <p className="text-black inline-block p-4 opacity-100 align-middle w-48 text-center" data-testid="serial-number">{ metricData.serial_number }</p>
-    <p className="text-black inline-block p-4 opacity-100 align-middle w-48 text-center" data-testid="date">{ date.toUTCString() }</p>
-    <p className="text-black inline-block p-4 opacity-100 align-middle w-48 text-center" data-testid="type">{ metricData.type.charAt(0).toLocaleUpperCase() + metricData.type.slice(1) }</p>
-    <p className="text-black inline-block p-4 opacity-100 align-middle w-48 text-center" data-testid="percentage">{ (parseFloat(metricData.percentage) * 100).toFixed(5) + '%' }</p>
+    <p className="text-white inline-block p-4 opacity-100 align-middle w-48 text-center" data-testid="serial-number">{ metricData.serial_number }</p>
+    <p className="text-white inline-block p-4 opacity-100 align-middle w-48 text-center" data-testid="date">{ date.toUTCString() }</p>
+    <p className="text-white inline-block p-4 opacity-100 align-middle w-48 text-center" data-testid="type">{ metricData.type.charAt(0).toLocaleUpperCase() + metricData.type.slice(1) }</p>
+    <p className="text-white inline-block p-4 opacity-100 align-middle w-48 text-center" data-testid="percentage">{ (parseFloat(metricData.percentage) * 100).toFixed(5) + '%' }</p>
     </div>
   <div className={ "flex place-content-between items-center "} data-testid="img-container">
   <FeedbackButton
